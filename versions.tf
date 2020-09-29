cc1304d4b4d1fb230233876685c94b409d16de02
jakemulley <jakemulley@users.noreply.github.com>
2020-09-29 11:42:00 +0100

    Change Auth0 provider source from Terraform to alexkappa

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 0.13"
   required_providers {
     auth0 = {
-      source = "terraform-providers/auth0"
+      source = "alexkappa/auth0"
     }
     aws = {
       source = "hashicorp/aws"
