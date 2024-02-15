327a469f1091754e6e4007224fcd51f2f4b7a1d8
David Sibley <david.sibley@digital.justice.gov.uk>
2024-02-15 13:48:31 +0000

    added constant for team memberships to be supplied as SAML assertion

--- auth0-actions/saml-mappings.js
+++ auth0-actions/saml-mappings.js
@@ -9,4 +9,9 @@ exports.onExecutePostLogin = async (event, api) => {
   // https://docs.aws.amazon.com/singlesignon/latest/userguide/troubleshooting.html
   api.samlResponse.setAttribute('http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress', `${event.user.nickname}${allowedDomain}`)
   api.samlResponse.setAttribute('http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name', `${event.user.nickname}${allowedDomain}`)
+
+  // Set SAML attribute for GitHub team
+  const userTeamsResponse = await octokit.request('GET /user/teams')
+  const userTeamSlugs = userTeamsResponse.data.map(team => team.slug)
+  api.samlResponse.setAttribute('https://aws.amazon.com/SAML/Attributes/PrincipalTag:github_team', userTeamSlugs.join(','))
 }
