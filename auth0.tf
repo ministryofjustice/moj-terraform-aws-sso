d38933c6999ad38d74d69bfa70caaba1d272f739
Aaron Robinson <41325732+ASTRobinson@users.noreply.github.com>
2024-11-29 10:00:33 +0000

    change to Microsoft

--- auth0.tf
+++ auth0.tf
@@ -75,7 +75,7 @@ resource "auth0_connection" "github_saml_connection" {
 }
 
 resource "auth0_connection" "azure_entraid_connection" {
-  display_name   = "Azure EntraID"
+  display_name   = "Microsoft"
   name           = "azure-entraid"
   strategy       = "waad"
   show_as_button = true
